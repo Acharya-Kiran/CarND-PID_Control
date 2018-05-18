0e51cbd23a8c2f90713ea7dcfdd55a2c919ade53
Kiran Acharya <38154597+Acharya-Kiran@users.noreply.github.com>
2018-05-18 17:50:23 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,148 @@
+#include <uWS/uWS.h>
+#include <iostream>
+#include "json.hpp"
+#include "PID.h"
+#include <math.h>
+
+// for convenience
+using json = nlohmann::json;
+
+// For converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+double min_x = -1.0;
+double max_x = 1.0;
+double tot_value = 0.0;
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+std::string hasData(std::string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.find_last_of("]");
+  if (found_null != std::string::npos) {
+    return "";
+  }
+  else if (b1 != std::string::npos && b2 != std::string::npos) {
+    return s.substr(b1, b2 - b1 + 1);
+  }
+  return "";
+}
+
+int main()
+{
+  uWS::Hub h;
+
+  PID pid;
+  // TODO: Initialize the pid variable.
+  pid.Init(0.2,0.0,1.5);
+  /*
+  The PID class is initialized with 3 variables,i.e. the Control Parameters for Propogation, Integral & Differential term.
+  The values are taken randomly out of experimentation.
+  
+  * Kp : 0.2 - the control parameter for Propogation.
+  The propogation parameter helps the car to move with respect to the cte( Cross Track Error ) provided by the simulator. The parameter is directly 
+  proportional with the steer_value, hence more the value more the vehicle tends to overshoot from the desired track.  
+  
+  * Ki : 0.0 - the control parameter for Integral.
+  I have not considered the Integral parameter as it degrades the performance by increasing the error of propogation.
+  
+  * Kd : 1.5- the control parameter for Differential.
+  The differential parameter helps to reduce the overshoot caused by the Propogation of the vehicle and hence help the vehicle to maintain the desired path.
+  
+  // The Kd value of 0.47 along with the throttle value of 0.05 works well in my system which has low processor.
+  */
+  
+
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    {
+      auto s = hasData(std::string(data).substr(0, length));
+      if (s != "") {
+        auto j = json::parse(s);
+        std::string event = j[0].get<std::string>();
+        if (event == "telemetry") {
+          // j[1] is the data JSON object
+          double cte = std::stod(j[1]["cte"].get<std::string>());
+          double speed = std::stod(j[1]["speed"].get<std::string>());
+          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          double steer_value;
+          /*
+          * TODO: Calcuate steering value here, remember the steering value is
+          * [-1, 1].
+          * NOTE: Feel free to play around with the throttle and speed. Maybe use
+          * another PID controller to control the speed!
+          */
+		  
+          pid.UpdateError(cte);
+		  
+		  //min_x and max_x are used to Normalize the steer_value and restrict them in range [-1,1].
+		  tot_value = pid.TotalError();
+		  if(tot_value<min_x){
+			  min_x = tot_value;
+		  }
+		  if(tot_value>max_x){
+			  max_x = tot_value;
+		  }
+		  //The following formula returns a value for steer_value in range [-1,1]. using the formula,
+		  // x' = (b-a)*(x-max_x)/(max_x-min_x)-1
+		  steer_value = 2*(tot_value-min_x)/(max_x-min_x)-1;
+		  
+          // DEBUG
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+
+          json msgJson;
+          msgJson["steering_angle"] = steer_value;
+          msgJson["throttle"] = 0.3;
+          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+          std::cout << msg << std::endl;
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }
+  });
+
+  // We don't need this since we're not using HTTP but if it's removed the program
+  // doesn't compile :-(
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+    const std::string s = "<h1>Hello world!</h1>";
+    if (req.getUrl().valueLength == 1)
+    {
+      res->end(s.data(), s.length());
+    }
+    else
+    {
+      // i guess this should be done more gracefully?
+      res->end(nullptr, 0);
+    }
+  });
+
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+    std::cout << "Connected!!!" << std::endl;
+  });
+
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
+  });
+
+  int port = 4567;
+  if (h.listen(port))
+  {
+    std::cout << "Listening to port " << port << std::endl;
+  }
+  else
+  {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  }
+  h.run();
+}
